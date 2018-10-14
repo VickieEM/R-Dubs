049f2d95d555800f1b76a3144c841f8bb3b3c8ce
Victoria Mendrinos <vmendrinos988@g.rwu.edu>
2018-10-14 10:12:41 -0400

    Add files via upload

---
+++ COMSC110 Notes Friday, October 12, 2018.java
@@ -0,0 +1,53 @@
+COMSC110 10/12/18
+
+	Objects
+
+	ex. Vending Machine 
+
+		State 
+			Demoniation of coins 
+			Type of snacks 
+			Prices 
+
+				Data Fields 
+
+
+		Behavior
+			Limits of how you can interact 
+
+
+Templates - recipies for Object
+	There is no main method - will not run
+	Just a template 
+
+Constructors
+	Have no return type (int or )
+	still work as Constructors
+		Need to define 
+		Need to find 
+
+only one will pe public class if but both are in the same file 
+	ex.
+		Class - Circle 
+		Public Class - Test Circle 
+
+Data Types - Primitive
+		int 
+		Double 
+		Byte 
+		Short
+		long 
+		Boolean 
+
+Declared  
+
+Int now new 
+
+Constructor has the same name as class 
+
+sin 
+
+
+
+
+
